77a36b5dce923318e6938cb51f1079099f5228eb
ubique-complex <bhardwajparas518@gmail.com>
2023-04-20 00:22:34 +0530

    third commit

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 const port = 3001
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
